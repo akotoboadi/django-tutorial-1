56e07e06ef84a94958228b91b7b0091e3e18830e
AkotoB <aboadi010@st.ug.edu.gh>
2024-11-15 02:47:02 +0000

    login and logout in django

--- newapp/urls.py
+++ newapp/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name='index'),
+    path('',views.greet1,name='greet1'),
     #path('<str:name>',views.greet,name ='greet'), this is what accepts a name as a route nd uses the name as a variable in the route.
-    path('greet1',views.greet1,name ='greet1'),
+    path('index',views.index,name ='index'),
     path('counter',views.counter,name ='counter'),
     path('register',views.register,name='register'),
+    path('login',views.login,name='login'),
+    path('logout',views.logout,name='logout'),
 
 
 
